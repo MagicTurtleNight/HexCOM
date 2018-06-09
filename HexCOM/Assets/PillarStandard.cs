db1d530a4ba2994f8cd8b0b6c326645227aad368
travelingnight <allanlm05@gmail.com>
2018-06-09 12:14:57 -0500

    Pillar movement (very basic test case)
    Adjusted PillarMove Script to move over time to three example positions.

---
+++ HexCOM/Assets/PillarStandard.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarStandard : MonoBehaviour {
+	
+	Vector3 initialPosition = new Vector3(0.0f, 5.0f, 0.0f);
+	/* The pillar currently has a height of ten units. I am setting it at a height of 5 as a middle point to move from.
+	 * Eventually we will want to avoid this.
+	 * We will probably set up the levels our selves and not want to care about where exactly it is, only that it goes up/down.
+	 * Once/if we do so, we will need to have the script find the starting position and just use that. I think.
+	 * */
+	Vector3 upwardPosition = new Vector3(0.0f, 10.0f, 0.0f);
+	Vector3 downwardPosition = new Vector3(0.0f, 0.0f, 0.0f);
+	//The desired positions 
+
+	Vector3 targetPosition;
+	float timeToReachTarget;
+
+	// Use this for initialization
+	void Start () {
+
+		transform.position = initialPosition;
+		//Edits position to starting position on initialization.
+		//Now also sets the target destination so no movement occurs accidentally.
+		//Debug.Log ("It's printing out"); it is
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		targetPosition = transform.position;
+
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			//Debug.Log ("Up Arrow");
+			targetPosition = upwardPosition;
+			StartCoroutine (MoveToPosition(transform, targetPosition, 3));
+
+		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			//Debug.Log ("Down Arrow");
+			targetPosition = downwardPosition;
+			StartCoroutine (MoveToPosition(transform, targetPosition, 3));
+
+		} else if (Input.GetKeyDown(KeyCode.O)) {
+			//Debug.Log ("Circle Arrow");
+			targetPosition = initialPosition;
+			StartCoroutine (MoveToPosition(transform, targetPosition, 3));
+
+		}
+
+	}
+
+	public IEnumerator MoveToPosition(Transform transform, Vector3 position, float timeToMove)
+	{
+		var currentPos = transform.position;
+		var t = 0f;
+		while(t < 1)
+		{
+			t += Time.deltaTime / timeToMove;
+			transform.position = Vector3.Lerp(currentPos, position, t);
+			yield return null;
+		}
+	}
+
+}
---
+++ HexCOM/Assets/TestScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+
+	Vector3 startPosition;
+	float t;
+	Vector3 target;
+	float timeToReachTarget;
+
+	void Start()
+	{
+		startPosition = new Vector3(0.0f, 0.0f, 0.0f);
+		target = new Vector3(0.0f, 5.0f, 0.0f);
+		transform.position = startPosition;
+		//setting the intitial position of the pillar to 0,0,0 -- Working just fine
+
+		StartCoroutine (MoveToPosition(transform, target, 3));
+	}
+
+	void Update() 
+	{
+		
+	}
+
+	public IEnumerator MoveToPosition(Transform transform, Vector3 position, float timeToMove)
+	{
+		var currentPos = transform.position;
+		var t = 0f;
+		while(t < 1)
+		{
+			t += Time.deltaTime / timeToMove;
+			transform.position = Vector3.Lerp(currentPos, position, t);
+			yield return null;
+		}
+	}
+}
